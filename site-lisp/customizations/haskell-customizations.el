6f9db92b59a28727a837a85f0b9bdecf42ffe64e
Matt Walker <matt.g.d.walker@gmail.com>
2017-12-15 13:18:27 -0500

    Revert "Setup Intero for Haskell."
    This reverts commit 47b55fbb58e0295a1e535340f3a43f39d793310e.

--- site-lisp/customizations/haskell-customizations.el
+++ site-lisp/customizations/haskell-customizations.el
@@ -3,27 +3,22 @@
 (add-path "~/.stack/snapshots/x86_64-linux-nopie/lts-8.23/8.0.2/share/x86_64-linux-ghc-8.0.2/HaRe-0.8.4.0/elisp")
 
 (require 'haskell-mode)
+(require 'haskell-interactive-mode)
 (require 'haskell-process)
-<<<<<<< HEAD
-(require 'hare)
-(require 'ghc)
-=======
->>>>>>> intero
 (require 'company)
+(require 'company-ghc)
 (require 'company-quickhelp)
 (require 'speedbar)
-(require 'intero)
 
-(add-to-list 'company-backends 'company-intero)
-(global-intero-mode)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 
+(add-to-list 'company-backends 'company-ghc)
+(custom-set-variables '(company-ghc-show-info t))
+
+(autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-debug "ghc" nil t)
 (add-hook 'haskell-mode-hook
 	  (lambda ()
-<<<<<<< HEAD
-	    (ghc-init)
-	    (hare-init)
-=======
->>>>>>> intero
 	    (company-mode 1)
 	    (company-quickhelp-mode 1)
 	    (local-set-key (kbd "C-M-d") 'company-show-doc-buffer)
@@ -32,6 +27,7 @@
 	    (turn-on-haskell-indentation)
 	    (turn-on-haskell-doc-mode)
 	    (turn-on-haskell-decl-scan)
+	    (setq haskell-tags-on-save t)
 	    (setq haskell-process-suggest-remove-import-lines t)
 	    (setq haskell-process-auto-import-loaded-modules t)
 	    (haskell-session-change)
